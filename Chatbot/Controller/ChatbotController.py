c2ff739a4938316dba62eda0209bec410a8e1ff5
husthunterpy01 <dangminhhust193231@gmail.com>
2024-12-28 00:05:51 +0700

    Implement the save data to mongodb of chat session

--- Chatbot/Controller/ChatbotController.py
+++ Chatbot/Controller/ChatbotController.py
@@ -1,15 +1,17 @@
 import os
-
+import shutil
 from dotenv import load_dotenv
 from fastapi import UploadFile, File, HTTPException, APIRouter
-import shutil
 from typing import List
-from pyparsing import empty
 from starlette.responses import JSONResponse
 from Chatbot.ChatbotService import generate_response,process_pdf
 from Chatbot.Model import QueryResponse
-from Chatbot.ChatbotService.vectordbHandlingService import create_search_index
-from Chatbot.utils.database import collection, MongoDBClient
+from Chatbot.ChatbotService.vectordbHandlingService import create_vectorsearch_index,create_search_index
+from Chatbot.utils.database import MongoDBClient
+from Chatbot.Model.ChatLog import Chatlog
+from Chatbot.Model.ChatSummarySession import ChatSummarizeSession
+from Chatbot.ChatbotService.chatlogService import saveChatConversation, saveSummaryBySession
+from Chatbot.ChatbotService.chatbotLogSummary import generate_summary
 
 load_dotenv()
 db_name = os.getenv("VECTOR_DB")
@@ -55,6 +57,7 @@ async def upload_pdf(files: List[UploadFile] = File(...)):
             process_pdf(file_path)
 
             if not MongoDBClient.list_search_indexes_checked(db_name, collection_name):
+                create_vectorsearch_index()
                 create_search_index()
 
             results.append({"file": file.filename, "status": "Processed successfully"})
@@ -66,4 +69,44 @@ async def upload_pdf(files: List[UploadFile] = File(...)):
         content={"results": results}
     )
 
+@router.post("user/summarizeChat/{session_id}")
+async def summarizeChatSession(session_id: str, sessionHistoryLog: str):
+    response = []
+    try:
+        generate_summary(sessionHistoryLog)
+        response.append({"session_id":session_id, "status":"Save session successfully"})
+    except Exception as e:
+        response.append({"session_id":session_id, "status":"Save session fail"})
 
+    return JSONResponse(
+        status_code=200,
+        content={"response": response}
+    )
+
+@router.post("user/saveChatHistoryBySession/{session_id}")
+async def saveChatHistoryBySession(session_id: str, sessionHistory: Chatlog):
+    response = []
+    try:
+        saveChatConversation(session_id, sessionHistory)
+        response.append({"session_id":session_id, "status":"Save session successfully"})
+    except Exception as e:
+        response.append({"session_id":session_id, "status":"Save session fail"})
+
+    return JSONResponse(
+        status_code=200,
+        content={"response": response}
+    )
+
+@router.post("user/saveChatSummaryBySession/{session_id}")
+async def saveChatSummaryBySession(session_id: str, savedChatSummary: ChatSummarizeSession):
+    response = []
+    try:
+        saveSummaryBySession(session_id, savedChatSummary)
+        response.append({"session_id":session_id, "status":"Save summary session successfully"})
+    except Exception as e:
+        response.append({"session_id":session_id, "status":"Save summary session fail"})
+
+    return JSONResponse(
+        status_code=200,
+        content={"response": response}
+    )
